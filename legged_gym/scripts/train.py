8fd03f8ce8d06bbc4635eeb461efc07891c6b579
MS06J <iamavery5417@gmail.com>
2025-12-15 17:10:10 +0100

    fix train.py

--- legged_gym/scripts/train.py
+++ legged_gym/scripts/train.py
@@ -48,11 +48,6 @@ import torch
 def train(args):
     env_cfg, train_cfg = task_registry.get_cfgs(name=args.task)
 
-    train_cfg.runner.run_name = 'WMP'
-
-    train_cfg.runner.max_iterations = 100000
-    train_cfg.runner.save_interval = 1000
-
     env, env_cfg = task_registry.make_env(name=args.task, args=args, env_cfg=env_cfg)
     ppo_runner, train_cfg = task_registry.make_wmp_runner(env=env, name=args.task, args=args, train_cfg=train_cfg)
     ppo_runner.learn(num_learning_iterations=train_cfg.runner.max_iterations, init_at_random_ep_len=True)
