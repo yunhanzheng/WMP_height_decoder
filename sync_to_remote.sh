2238bd6ff90c6b742016014e5a9845b574573df6
MS06J <iamavery5417@gmail.com>
2025-12-15 16:27:53 +0100

    add script to sync to remote

---
+++ sync_to_remote.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Sync WMP directory to remote server
+# Remote: yulong@131.159.60.170
+
+REMOTE_USER="yulong"
+REMOTE_HOST="131.159.60.170"
+REMOTE_PATH="~/student_projects/wenshuo_rl/WMP"
+LOCAL_PATH="/home/hsu/WMP"
+
+echo "Syncing ${LOCAL_PATH} to ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}"
+
+rsync -avz --progress \
+    --exclude '.git' \
+    --exclude '__pycache__' \
+    --exclude '*.pyc' \
+    --exclude '.venv' \
+    --exclude 'venv' \
+    --exclude '.idea' \
+    --exclude '.vscode' \
+    --exclude '*.egg-info' \
+    --exclude 'logs' \
+    --exclude 'wandb' \
+    --exclude '.ipynb_checkpoints' \
+    --exclude 'build' \
+    --exclude 'dist' \
+    "${LOCAL_PATH}/" "${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}"
+
+echo "Sync complete!"
