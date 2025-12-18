1ac0c05715e4dfd280ca83bd96b2a7dc6a4872ff
MS06J <iamavery5417@gmail.com>
2025-12-18 15:22:07 +0100

    add go2 baseline blind

---
+++ legged_gym/envs/go2_baseline/go2_baseline_blind_config.py
@@ -0,0 +1,281 @@
+from legged_gym.envs.base.legged_robot_config import LeggedRobotCfg, LeggedRobotCfgPPO
+
+class GO2BlindRoughCfg( LeggedRobotCfg ):
+    class env(LeggedRobotCfg.env):
+        num_envs = 4096
+        include_history_steps = None  # Number of steps of history to include.
+        prop_dim = 33 # proprioception
+        action_dim = 12
+        privileged_dim = 0  # privileged_obs[:,:privileged_dim] is the privileged information in privileged_obs, include 3-dim base linear vel
+        height_dim = 0  # privileged_obs[:,-height_dim:] is the heightmap in privileged_obs
+        forward_height_dim = 0 # for depth image prediction
+        num_observations = prop_dim + action_dim
+        num_privileged_obs = None  # Set to None for symmetric training (no privileged observations)
+        privileged_obs = False  # Disable extra privileged observations (domain randomization params)
+
+    class terrain(LeggedRobotCfg.terrain):
+        # domino terrain
+        terrain_proportions = [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0]
+
+        border_size = 25
+        mesh_type = "trimesh"
+        terrain_length = 15.0
+        terrain_width = 15.0
+        num_rows = 10  # number of terrain rows (levels)
+        num_cols = 10  # number of terrain cols (types)
+        measure_heights = False
+        max_init_terrain_level = 5  # starting curriculum state
+
+        measured_points_x = [
+            -0.8,
+            -0.7,
+            -0.6,
+            -0.5,
+            -0.4,
+            -0.3,
+            -0.2,
+            -0.1,
+            0.0,
+            0.1,
+            0.2,
+            0.3,
+            0.4,
+            0.5,
+            0.6,
+            0.7,
+            0.8,
+        ]  # 1mx1.6m rectangle (without center line)
+        measured_points_y = [-0.5, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.5]
+        map_path = None
+        num_points = len(measured_points_x) * len(measured_points_y)
+        measured_forward_points_x = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0,
+                                     1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9,
+                                     2.0]  # 1mx1.6m rectangle (without center line)
+        measured_forward_points_y = [-1.2, -1.1, -1.0, -0.9, -0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1, 0.,
+                                     0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2]
+
+    class init_state( LeggedRobotCfg.init_state ):
+        pos = [0.0, 0.0, 0.42] # x,y,z [m]
+        default_joint_angles = { # = target angles [rad] when action = 0.0
+            'FL_hip_joint': 0.1,   # [rad]
+            'RL_hip_joint': 0.1,   # [rad]
+            'FR_hip_joint': -0.1 ,  # [rad]
+            'RR_hip_joint': -0.1,   # [rad]
+
+            'FL_thigh_joint': 0.8,     # [rad]
+            'RL_thigh_joint': 1.,   # [rad]
+            'FR_thigh_joint': 0.8,     # [rad]
+            'RR_thigh_joint': 1.,   # [rad]
+
+            'FL_calf_joint': -1.5,   # [rad]
+            'RL_calf_joint': -1.5,    # [rad]
+            'FR_calf_joint': -1.5,  # [rad]
+            'RR_calf_joint': -1.5,    # [rad]
+        }
+    
+    class sim:
+        dt = 0.005
+        substeps = 1
+        gravity = [0., 0., -9.81]  # [m/s^2]
+        up_axis = 1  # 0 is y, 1 is z
+
+        class physx:
+            num_threads = 10
+            solver_type = 1  # 0: pgs, 1: tgs
+            num_position_iterations = 4
+            num_velocity_iterations = 0
+            contact_offset = 0.01  # [m]
+            rest_offset = 0.0  # [m]
+            bounce_threshold_velocity = 0.5  # 0.5 [m/s]
+            max_depenetration_velocity = 1.0
+            max_gpu_contact_pairs = 2 ** 23  # 2**24 -> needed for 8000 envs and more
+            default_buffer_size_multiplier = 5
+            contact_collection = 2  # 0: never, 1: last sub-step, 2: all sub-steps (default=2)
+
+    class control( LeggedRobotCfg.control ):
+        # PD Drive parameters:
+        control_type = 'P'
+        stiffness = {'joint': 20.}  # [N*m/rad]
+        damping = {'joint': 0.5}     # [N*m*s/rad]
+        # action scale: target angle = actionScale * action + defaultAngle
+        action_scale = 0.25
+        # decimation: Number of control action updates @ sim DT per policy DT
+        decimation = 4
+
+    class depth:
+        use_camera = False
+        camera_num_envs = 1024
+        camera_terrain_num_rows = 10
+        camera_terrain_num_cols = 20
+
+        position = [0.33, 0.0, 0.08]  # front camera
+        y_angle = [-5, 5]  # positive pitch down
+        z_angle = [0, 0]
+        x_angle = [0, 0]
+
+        update_interval = 5  # 5 works without retraining, 8 worse
+
+        original = (64, 64)
+        resized = (64, 64)
+        horizontal_fov = 58
+        buffer_len = 2
+
+        near_clip = 0
+        far_clip = 2
+        dis_noise = 0.0
+
+        scale = 1
+        invert = True
+
+    class asset( LeggedRobotCfg.asset ):
+        file = '{LEGGED_GYM_ROOT_DIR}/resources/robots/go2/urdf/go2.urdf'
+        name = "go2"
+        foot_name = "foot"
+        penalize_contacts_on = ["thigh", "calf"]
+        terminate_after_contacts_on = ["base"]
+        self_collisions = 1 # 1 to disable, 0 to enable...bitwise filter
+
+    class domain_rand:
+        randomize_friction = True
+        friction_range = [0.5, 2.0]
+        randomize_restitution = True
+        restitution_range = [0.0, 0.0]
+
+        randomize_base_mass = True
+        added_mass_range = [0., 3.]  # kg
+        randomize_link_mass = True
+        link_mass_range = [0.8, 1.2]
+        randomize_com_pos = True
+        com_x_pos_range = [-0.05, 0.05]
+        com_y_pos_range = [-0.05, 0.05]
+        com_z_pos_range = [-0.05, 0.05]
+
+        push_robots = True
+        push_interval_s = 15
+        min_push_interval_s = 15
+        max_push_vel_xy = 1.0
+
+        randomize_gains = True
+        stiffness_multiplier_range = [0.8, 1.2]
+        damping_multiplier_range = [0.8, 1.2]
+        randomize_motor_strength = True
+        motor_strength_range = [0.8, 1.2]
+        randomize_action_latency = True
+        latency_range = [0.00, 0.005]
+
+    class normalization:
+        class obs_scales:
+            lin_vel = 1.0
+            ang_vel = 0.25
+            dof_pos = 1.0
+            dof_vel = 0.05
+            # privileged
+            height_measurements = 5.0
+            contact_force = 0.005
+            com_pos = 20
+            pd_gains = 5
+
+        clip_observations = 100.
+        clip_actions = 100.0
+        base_height = 0.3 # base height of A1, used to normalize measured height
+
+    class noise:
+        add_noise = False
+        noise_level = 1.0  # scales other values
+
+        class noise_scales:
+            dof_pos = 0.01
+            dof_vel = 1.5
+            lin_vel = 0  # set lin_vel as privileged information
+            ang_vel = 0.2
+            gravity = 0.05
+            height_measurements = 0  # only for critic
+
+    class rewards(LeggedRobotCfg.rewards):
+        reward_curriculum = False
+        class scales:
+            termination = -0.1
+            tracking_lin_vel = 1.0
+            tracking_ang_vel = 0.5
+            lin_vel_z = -0.1
+            ang_vel_xy = -0.01
+            orientation = -0.01
+            dof_acc = -2.5e-7
+            joint_power = -2e-5
+            base_height = -0.0
+            foot_clearance = 0.0
+            action_rate = -0.0002
+            smoothness = -0.0001
+            feet_air_time = 0.01
+            collision = -0.1
+            feet_stumble = -0.5
+            stand_still = -0.01  # Penalize motion at zero commands
+            torques = -0.0
+            dof_vel = -0.0
+            dof_pos_limits = -0.0
+            dof_vel_limits = -0.0
+            torque_limits = -0.0
+
+        only_positive_rewards = (
+            False  # if true negative total rewards are clipped at zero (avoids early termination problems)
+        )
+        tracking_sigma = 0.25  # tracking reward = exp(-error^2/sigma)
+        soft_dof_pos_limit = 1.0  # percentage of urdf limits, values above this limit are penalized
+        soft_dof_vel_limit = 1.0
+        soft_torque_limit = 1.0
+        base_height_target = 0.30
+        max_contact_force = 100.0  # forces above this value are penalized
+        clearance_height_target = -0.1
+        
+    class commands:
+        curriculum = False
+        max_lin_vel_forward_x_curriculum = 1.0
+        max_lin_vel_backward_x_curriculum = 0.0
+        max_lin_vel_y_curriculum = 0.0
+        max_ang_vel_yaw_curriculum = 1.0
+
+        num_commands = 4  # default: lin_vel_x, lin_vel_y, ang_vel_yaw, heading (in heading mode ang_vel_yaw is recomputed from heading error)
+        resampling_time = 10.  # time before command are changed[s]
+        heading_command = True  # if true: compute ang vel command from heading error
+
+        class ranges:
+            lin_vel_x = [-1.0, 1.0]  # min max [m/s]
+            lin_vel_y = [-1.0, 1.0]  # min max [m/s]
+            ang_vel_yaw = [-3.14, 3.14]  # min max [rad/s]
+            heading = [-3.14, 3.14]  # min max [rad/s]
+
+class GO2BlindRoughCfgPPO( LeggedRobotCfgPPO ):
+    runner_class_name = 'WMPRunner'
+
+    class policy:
+        init_noise_std = 1.0
+        encoder_hidden_dims = [256, 128]
+        wm_encoder_hidden_dims = [64, 64]
+        actor_hidden_dims = [256, 128, 64]
+        critic_hidden_dims = [512, 256, 128]
+        latent_dim = 32 + 3
+        wm_latent_dim = 32
+        activation = 'elu'  # can be elu, relu, selu, crelu, lrelu, tanh, sigmoid
+
+    class algorithm( LeggedRobotCfgPPO.algorithm ):
+        entropy_coef = 0.01
+        # vel_predict_coef = 1.0
+        num_learning_epochs = 5
+        num_mini_batches = 4
+
+    class runner( LeggedRobotCfgPPO.runner ):
+        run_name = ''
+        experiment_name = 'go2_blind'
+        algorithm_class_name = 'PPO'
+        policy_class_name = 'ActorCritic'
+        max_iterations = 20000  # number of policy updates
+        save_interval = 1000
+        use_wandb = True
+
+    class depth_predictor:
+        lr = 3e-4
+        weight_decay = 1e-4
+        training_interval = 10
+        training_iters = 1000
+        batch_size = 1024
+        loss_scale = 100
