967c83750ca630f43df095bc04c00d80eda15ff2
MS06J <iamavery5417@gmail.com>
2026-02-01 19:50:00 +0100

    fix previous commit for go2_blind

--- legged_gym/envs/base/legged_robot.py
+++ legged_gym/envs/base/legged_robot.py
@@ -497,8 +497,9 @@ class LeggedRobot(BaseTask):
                 self.obs_buf = self.privileged_obs_buf[:, 6:]
             elif self.num_obs == self.num_privileged_obs - 3:
                 self.obs_buf = self.privileged_obs_buf[:, 3:]
-            elif hasattr(self.cfg.env, 'height_dim') and self.cfg.terrain.measure_heights:
+            elif hasattr(self.cfg.env, 'height_dim') and self.cfg.terrain.measure_heights and self.num_obs != self.num_privileged_obs:
                 # go2_baseline style: actor gets base_lin_vel + proprioception + heightmap + actions
+                # Only use this path when num_obs != num_privileged_obs (asymmetric actor-critic)
                 # obs_with_vel layout: lin_vel(3) + ang_vel(3) + gravity(3) + commands(3) + dof_pos(12) + dof_vel(12) + actions(12) = 48
                 # We want: lin_vel(3) + ang_vel(3) + gravity(3) + commands(3) + dof_pos(12) + dof_vel(12) + heightmap(187) + actions(12)
                 prop_with_vel = obs_with_vel[:, :36]  # lin_vel(3) + ang_vel(3) + gravity(3) + commands(3) + dof_pos(12) + dof_vel(12)
