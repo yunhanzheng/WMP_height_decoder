6530823a9470b7cf7cf1d978c00b7d94da8423e2
hsu <iamavery5417@gmail.com>
2026-02-24 16:06:58 +0100

    import fix

--- legged_gym/utils/__init__.py
+++ legged_gym/utils/__init__.py
@@ -29,6 +29,7 @@
 # Copyright (c) 2021 ETH Zurich, Nikita Rudin
 
 from .helpers import class_to_dict, get_load_path, get_args, export_policy_as_jit, set_seed, update_class_from_dict
+from .wmp_exported import export_wmp
 from .task_registry import task_registry
 from .logger import Logger
 from .math import *
